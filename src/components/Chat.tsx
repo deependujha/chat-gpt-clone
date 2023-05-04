3418e2afdc9aa3c6089d55616c414f255a6adefb
deependujha <deependujha21@gmail.com>
2023-05-04 23:49:43 +0530

    navbar part done

---
+++ src/components/Chat.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Chat = () => {
+	return (
+		<div className="flex flex-col h-screen w-screen bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-500"></div>
+	);
+};
+
+export default Chat;
